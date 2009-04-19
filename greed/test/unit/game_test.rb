b24cd8c683b067219b61b88a7b8cd62944685eb7
Jim Weirich <jim.weirich@gmail.com>
2009-04-19 19:31:17 -0400

    Added players method on game to return all players.

--- greed/test/unit/game_test.rb
+++ greed/test/unit/game_test.rb
@@ -2,8 +2,15 @@ require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
   context 'A game' do
-    should 'be created' do
-      assert_not_nil Game.new      
+    setup do
+      @game = Game.new
+      @cp = ComputerPlayer.new
+      @hp = HumanPlayer.new
+      @game.computer_players = [@cp]
+      @game.human_player = @hp
+    end
+    should 'combine its computer players and human player' do
+      assert_equal [@cp, @hp], @game.players
     end
   end
 end
