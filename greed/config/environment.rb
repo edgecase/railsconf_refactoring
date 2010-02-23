e35dfd0b83377e3cf4325c33941b5595fa874b6a
Joe O'Brien <joe@theedgecase.com>
2010-02-22 17:16:17 -0800

    adding in changes for rails to work

--- greed/config/environment.rb
+++ greed/config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,11 +19,11 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "flexmock", :version => "0.8.6"
-  config.gem "thoughtbot-shoulda", :version => "2.10.1", :lib => "shoulda"
-  config.gem "cucumber", :version => "0.4.2"
-  config.gem "polyglot", :version => "0.2.9"
-  config.gem "webrat", :version => "0.4.4"
+  config.gem "flexmock"#, :version => "0.8.6"
+#  config.gem "shoulda"#, :version => "2.10.1", :lib => "shoulda"
+  config.gem "cucumber"#, :version => "0.4.2"
+  config.gem "polyglot"#, :version => "0.2.9"
+  config.gem "webrat"#, :version => "0.4.4"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
